7ca53561ec870ef1a8ecee935cf01a44c66085a1
momeyer <meyer.monique93@gmail.com>
2021-05-03 10:05:32 +0200

    Added more integration tests

--- src/checker/main.c
+++ src/checker/main.c
@@ -8,7 +8,7 @@ int	main(int argc, const char *argv[])
 	t_status	ret;
 
 	if (argc < 2)
-		return (1);
+		return (ERROR);
 	ret = run_checker(argc - 1, &(argv[1]), get_next_instruction);
 	print_message(ret);
 	exit(ret);
--- test/integration_test.py
+++ test/integration_test.py
@@ -7,6 +7,7 @@ from enum import IntEnum
 
 LIGHT_RED = "\033[1;31m"
 LIGHT_GREEN = "\033[1;32m"
+LIGHT_PURPLE = "\033[1;35m"
 LIGHT_WHITE = "\033[0;37m"
 RUN_COMMAND = "./push_swap {} | ./checker {}"
 EXPECT_ERROR = "Expect:\n\033[1;31mError\033[0;37m"
@@ -22,11 +23,17 @@ class Status(IntEnum):
     @classmethod
     def print(cls, expected):
         if expected == cls.OK:
-            print(EXPECT_KO)
+            print(EXPECT_OK)
         elif expected == cls.KO:
             print(EXPECT_KO)
         else:
             print(EXPECT_ERROR)
+
+
+def printTestBanner(testName):
+    print("{}======================================================================{}".format(LIGHT_PURPLE, LIGHT_WHITE))
+    print("                          {}".format(testName))
+    print("{}======================================================================{}".format(LIGHT_PURPLE, LIGHT_WHITE))
 
 
 class CheckerErrorManagement:
@@ -63,30 +70,29 @@ class CheckerErrorManagement:
         self._runCheckerInput("Numeric value smaller than INT_MIN", "1 2 -2147483649", Status.ERROR)
 
     def _testWithoutAnyInput(self):
-        self._runCheckerInput("Whitout any parameters", None, Status.ERROR)
+        print("\nWhitout any parameters, should just return")
+        assert subprocess.run("./checker", shell=True).returncode > 0
+        print("\n")
 
     def _testInvalidInstruction(self):
-        self._runCheckerInstruction("Valid parameters, invalid instruction", "r", expected=Status.ERROR)
+        self._runCheckerInstruction("Valid parameters, invalid instruction", "1 2 3", "r", Status.ERROR)
 
     def _testSpacesBeforeInstruction(self):
-        self._runCheckerInstruction("Valid parameters, valid instruction with spaces before", inp="1 2 3", instruction="  rra", expected=Status.ERROR)
+        self._runCheckerInstruction("Valid instruction with spaces before", "1 2 3", "  rra", Status.ERROR)
 
     def _testSpacesAfterInstruction(self):
-        self._runCheckerInstruction("Valid parameters, valid instruction with spaces after", inp="1 2 3", instruction="rra   ", expected=Status.ERROR)
+        self._runCheckerInstruction("Valid instruction with spaces after", "1 2 3", "rra   ", Status.ERROR)
 
     def runTests(self):
-        print("-------Input Parser Tests-------")
+        printTestBanner("Checker Error management")
         self._testNonNumericParameters()
         self._testDuplicatedNumericParameters()
         self._testNumericValueBiggerThanIntMax()
         self._testNumericValueSmallerThanIntMin()
         self._testWithoutAnyInput()
-        print("--------------------------------")
-        print("----Instruction Parser Tests----")
         self._testInvalidInstruction()
         self._testSpacesBeforeInstruction()
         self._testSpacesAfterInstruction()
-        print("--------------------------------")
 
 
 class CheckerFalseTests:
@@ -102,50 +108,36 @@ class CheckerFalseTests:
         ret = subprocess.run(cmd, shell=True)
         assert ret.returncode == int(expected)
 
-    def _testWrongSetOfInstructions(self):
-        self._runCheckerInstruction("Valid parameters, wrong sorting instructions", inp="0 9 1 8 2 7 3 6 4 5", instruction="sa\npb\nrrr", expected=Status.KO)
+    def _testWrongSetOfInstructions(self, inp, instructions):
+        self._runCheckerInstruction("Valid parameters, wrong sorting instructions", inp, instructions, Status.KO)
 
     def runTests(self):
-        print("------Checker False tests------")
-        self._testWrongSetOfInstructions()
-        print("-------------------------------")
+        printTestBanner("Checker False tests")
+        self._testWrongSetOfInstructions("0 9 1 8 2 7 3 6 4 5", "sa\npb\nrrr")
+        self._testWrongSetOfInstructions("5 1 2 3 4", "pb")
+        # self._testWrongSetOfInstructions("1 2 3 4", "pa")
 
 
-def generateStack(size, rangeStart, rangeEnd):
-    stack = range(rangeStart, rangeEnd)
-    return random.sample(stack, size)
+class CheckerRightTests:
 
+    def __init__(self):
+        pass
 
-def printStack(stack):
-    print("before: {}".format(stack))
-    print("after: {}".format(sorted(stack)))
+    def _runCheckerInstruction(self, testName, inp=None, instruction=None, expected=Status.OK):
+        print("\nTest case: " + testName)
+        Status.print(expected)
+        print("Got:   ")
+        cmd = "printf '{}' | ./checker {}".format(instruction, inp)
+        ret = subprocess.run(cmd, shell=True)
+        assert ret.returncode == int(expected)
 
+    def _testRightSetOfInstructions(self, inp, instructions):
+        self._runCheckerInstruction("Right sorting instructions", inp, instructions, Status.OK)
 
-def convertToIntList(string):
-    strList = string.split(" ")
-    strList.remove("")
-    intList = [int(i) for i in strList]
-    return intList
-
-
-def convertToStrList(intsList):
-    stringInts = [str(i) for i in intsList]
-    strOfInts = " ".join(stringInts)
-    return strOfInts
-
-
-def testManyInputs(listMin, listMax):
-    for i in range(listMin, listMax):
-        inputStack = generateStack(i, -2147483648, 2147483647)
-        inputList = convertToStrList(inputStack)
-        print("list size: {}".format(i))
-        ret = subprocess.run(RUN_COMMAND.format(inputList, inputList), shell=True)
-        assert ret.returncode == 0
-        with open("result") as f:
-            line = f.read()
-            output = convertToIntList(line)
-        expected = sorted(inputStack)
-        assert output == expected
+    def runTests(self):
+        printTestBanner("Checker Right tests")
+        self._testRightSetOfInstructions("0 1 2", "")
+        self._testRightSetOfInstructions("0 9 1 8 2", "pb\nra\npb\nra\nsa\nra\npa\npa")
 
 
 def main():
@@ -155,6 +147,45 @@ def main():
 
     CheckerErrorManagement().runTests()
     CheckerFalseTests().runTests()
-    
+    CheckerRightTests().runTests()
+
 
 main()
+
+
+# def generateStack(size, rangeStart, rangeEnd):
+#     stack = range(rangeStart, rangeEnd)
+#     return random.sample(stack, size)
+
+
+# def printStack(stack):
+#     print("before: {}".format(stack))
+#     print("after: {}".format(sorted(stack)))
+
+
+# def convertToIntList(string):
+#     strList = string.split(" ")
+#     strList.remove("")
+#     intList = [int(i) for i in strList]
+#     return intList
+
+
+# def convertToStrList(intsList):
+#     stringInts = [str(i) for i in intsList]
+#     strOfInts = " ".join(stringInts)
+#     return strOfInts
+
+
+# def testManyInputs(listMin, listMax):
+#     for i in range(listMin, listMax):
+#         inputStack = generateStack(i, -2147483648, 2147483647)
+#         inputList = convertToStrList(inputStack)
+#         print("list size: {}".format(i))
+#         ret = subprocess.run(RUN_COMMAND.format(inputList, inputList), shell=True)
+#         assert ret.returncode == 0
+#         with open("result") as f:
+#             line = f.read()
+#             output = convertToIntList(line)
+#         expected = sorted(inputStack)
+#         assert output == expected
+
